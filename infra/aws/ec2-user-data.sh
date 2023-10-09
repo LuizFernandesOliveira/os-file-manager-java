b91f1600b09685366013925cb9b359c65fa977d4
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2023-10-08 23:15:00 -0300

    feat: add ec2-user-data configs

---
+++ infra/aws/ec2-user-data.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker $USER
+sudo chmod 666 /var/run/docker.sock
+
+export AWS_ACCESS_KEY_ID=asdasdasd
+export AWS_SECRET_ACCESS_KEY=asdasdasdas
+
+# Init Service
+docker run --name=os-file-manager senioritymeter/os-file-manager:latest \
+  -p 80:8080 \
+  -e AWS_S3_BUCKET_NAME=seniority-meter \
+  -e AWS_SECRET_KEY=AWS_ACCESS_KEY_ID \
+  -e AWS_ACCESS_KEY=AWS_SECRET_ACCESS_KEY \
+
