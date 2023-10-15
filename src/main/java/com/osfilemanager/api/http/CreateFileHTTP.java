9fbddca5e3a630668eb012cb3ea8f563fb243f30
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2023-10-15 11:48:13 -0300

    feat: adjusts cross origins

--- src/main/java/com/osfilemanager/api/http/CreateFileHTTP.java
+++ src/main/java/com/osfilemanager/api/http/CreateFileHTTP.java
@@ -6,14 +6,14 @@ import com.osfilemanager.interactions.input.CreateInput;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(
+    origins = "*",
+    methods = {RequestMethod.POST})
 public class CreateFileHTTP implements FileHTTP {
   private final FileCreate interaction;
 
